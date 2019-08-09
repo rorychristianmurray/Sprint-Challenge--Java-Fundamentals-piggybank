05cbee791fc96dc3c8a4d8c9b0ec44fffb77b059
Rory Murray <rory.christian.murray@gmail.com>
2019-08-09 11:51:14 -0400

    changed ArrayList inputs to README

--- src/piggyBankApp/Main.java
+++ src/piggyBankApp/Main.java
@@ -14,7 +14,13 @@ public class Main {
 
         // add items to ArrayList
         piggyBank.add(new Dollar());
+        piggyBank.add(new Quarter());
+        piggyBank.add(new Dime());
+        piggyBank.add(new Dollar(5));
+        piggyBank.add(new Nickel(3));
+        piggyBank.add(new Dime(7));
         piggyBank.add(new Dollar());
+        piggyBank.add(new Penny(10));
 
         // Output
         System.out.println("*** OUTPUT ***");
